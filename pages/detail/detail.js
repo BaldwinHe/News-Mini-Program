d488b3825aaebeeca442499553789b9971181406
BaldwinHe <heyanbai1999@gmail.com>
2018-10-22 20:06:30 +0800

    Get Imformation Online
    从网络获取数据

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -5,44 +5,59 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    id:"0",
+    title:"",
+    from:"中国新闻网",
+    date:"09:34",
+    read:"",
+    content:"",
+    imagePath:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    this.setData({
+      id : options.id
+    })
+    this.getDetail()
+  },
+  getDetail(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data:{
+        id: this.data.id
+      },
+      success: res =>{
+        let result = res.data.result;
+        this.showDetail(result);
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
   },
 
+  showDetail(result){
+    if (result.source == "") result.source = "未知来源";
+    if (result.date == "") result.date = "未知时间";
+    else result.date = result.date.substring(11, 16);
+    this.setData({
+      title:result.title,
+      read: "阅读数 : " + result.readCount,
+      imagePath:result.firstImage,
+      date:result.date,
+      from:result.source
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,12 +2,69 @@
 //获取应用实例
 
 Page({
+  data:{
+    type: "gn",
+    firstImage: "/images/Unknown.jpg",
+    firstTitle:"请检查网络连接",
+    firstFrom:"Unknown",
+    firstDate:"",
+    listNews: [],
+    firstID:0,
+  },
+  onPullDownRefresh(){
 
-
-
-  getNewsDetail(){
+  },
+  onLoad(){
+    this.getNews();
+  },
+  getNews(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: this.data.type
+      },
+      success: res=> {
+        let result = res.data.result;
+        this.setFirst(result[0]);
+        this.setList(result.slice(1));
+      },
+      complete: ()=> {
+        callback && callback()
+      }
+    })
+  },
+  setFirst(result){
+    if(result.source == "") result.source = "未知来源";
+    if(result.date == "") result.date = "未知时间";
+    else result.date = result.date.substring(11,16);
+    this.setData({
+      firstImage:result.firstImage,
+      firstTitle:result.title,
+      firstFrom:result.source,
+      firstDate:result.date,
+      firstID:result.id
+    })
+  },
+  setList(result){
+    let listNews = [];
+    for(let i = 0;i<result.length;i++){
+      if (result[i].source == "") result[i].source = "未知来源";
+      if (result[i].date == "") result[i].date = "未知时间";
+      else result[i].date = result[i].date.substring(11, 16);
+      listNews.push({
+        title: result[i].title,
+        from: result[i].source,
+        date: result[i].date,
+        image: result[i].firstImage,
+        id : result[i].id
+      })
+    }
+    this.setData({ listNews:listNews });
+  },
+  getNewsDetail :function(event){
+    let ID = event.currentTarget.dataset.id;
     wx.navigateTo({
-      url: '/pages/detail/detail',
+      url: '/pages/detail/detail?id=' + ID,
     })
   }
 })
