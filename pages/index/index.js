796b22652adf1bfc17185a1eb985fad2e4e433dd
BaldwinHe <heyanbai1999@gmail.com>
2018-10-24 11:42:51 +0800

    下拉刷新/类别切换
    可进行和下拉刷新/类别切换

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,24 @@
 //index.js
 //获取应用实例
-
+const typeMap = {
+  "国内": "gn",
+  "国际": "gj",
+  "财经": "cj",
+  "娱乐": "yl",
+  "军事": "js",
+  "体育": "ty",
+  "其他": "other"
+}
+const typeID = {
+  "国内": 0,
+  "国际": 1,
+  "财经": 2,
+  "娱乐": 3,
+  "军事": 4,
+  "体育": 5,
+  "其他": 6
+}
+const typeName = ["国内", "国际", "财经", "娱乐", "军事", "体育", "其他"]; 
 Page({
   data:{
     type: "gn",
@@ -10,11 +28,25 @@ Page({
     firstDate:"",
     listNews: [],
     firstID:0,
+    newsType: "",
+    theSelectType: 0
   },
   onPullDownRefresh(){
-
+    this.getNews(() => {
+      wx.stopPullDownRefresh()
+    })
   },
   onLoad(){
+    let newsList = [];
+    for (let k = 0; k <= 6; k++) {
+      newsList.push({
+        name: typeName[k],
+        id: k
+      })
+    }
+    this.setData({
+      newsType: newsList
+    })
     this.getNews();
   },
   getNews(callback){
@@ -66,5 +98,15 @@ Page({
     wx.navigateTo({
       url: '/pages/detail/detail?id=' + ID,
     })
-  }
+  },
+  selectType: function (event) {
+    let ans = event.currentTarget.dataset.id
+    let theType = typeMap[ans.name];
+    let theID = ans.id;
+    this.setData({
+      type: theType,
+      theSelectType: theID
+    })
+    this.getNews();
+  } 
 })
